7c271ced2ae164c8e915476aa775b8101fdce28a
yesicalanata-afk <yesicalanata@gmail.com>
2026-06-18 19:16:55 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 # LEER VARIABLES DE RAILWAY
-TOKEN = "TU_TOKEN_DE_TELEGRAM_AQUÍ"
-CLASH_KEY = "TU_CLASH_API_KEY_AQUÍ"
+TOKEN = "8641151810:AAFcj8ICA9-w5zG9sR3dVny5g3PK--qmNNk"
+CLASH_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjUwYTNiZDVkLTgwM2MtNDdkNC1iNTg2LTJkOTg3N2U3NTk4NyIsImlhdCI6MTc4MTgxOTU1Mywic3ViIjoiZGV2ZWxvcGVyL2ZlNGZkZGUwLTlk ZmUtMzVhYi0zYmE1LWI1ZmFjN2UyYmVlMCIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjAuMC4wLjAiXSwidHlwZSI6ImNsaWVudCJ9XX0.ZyR4o5MRyZT9ICP9vGUTyoVxGLPhX7OUJAm2TBw8j8R41N80OGXwtNXO-cHn1N5Ys2bBT7EH1lDcJNa164RKEg"
 
 if not TOKEN or not CLASH_KEY:
     raise ValueError("ERROR: Falta TELEGRAM_TOKEN o CLASH_API_KEY en las variables de Railway.")
